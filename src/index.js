4c2f3bcc77d6201641d66eca00adcd5b81afcd65
lu1151 <luxi.li@outlook.com>
2020-11-01 23:42:22 -0500

    11/01Commit2

--- src/index.js
+++ src/index.js
@@ -2,25 +2,44 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css"
 
+// setup vars
+const firstBook = {
+    img: "https://m.media-amazon.com/images/I/81T1F3Dv0YL._SX140_.jpg",
+    title: "Harry Potter",
+    author: "J.K. Rowling"
+}
+
+const secondBook = {
+    img: "https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg",
+    title: "A Promised Land",
+    author: "Barack Obama"
+}
+
 function BookList() {
     return (
         <section className="booklist">
-            <Book />
+            <Book
+                img={firstBook.img}
+                title={firstBook.title}
+                author={firstBook.author}
+            />
+            <Book
+                img={secondBook.img}
+                title={secondBook.title}
+                author={secondBook.author}
+            />
         </section>
     )
 }
 
-
-
-const Book = () => {
-    const title = "Harry Potter and the Order of the Phoenix";
-    const author = "J.K. Rowling";
-    return (<article className="book">
-        <img src="https://m.media-amazon.com/images/I/81T1F3Dv0YL._SX140_.jpg" alt="" />
-        <h3>{title}</h3>
-        By <span>{author.toLowerCase()}</span>
-        <p>{}</p>
-    </article>)
+const Book = ({ img, title, author }) => {
+    // const { img, title, author } = props;
+    return (
+        <article className="book">
+            <img src={img} alt="" />
+            <h3>{title}</h3>
+        By <span>{author}</span>
+        </article>)
 }
 
 ReactDom.render(<BookList />, document.getElementById("root"));
