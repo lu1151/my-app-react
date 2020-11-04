bbcc8a562d2cc068b60301f85c7cb99cace3903b
lu1151 <luxi.li@outlook.com>
2020-11-04 00:06:44 -0500

    10/4Commit

--- src/index.js
+++ src/index.js
@@ -3,37 +3,40 @@ import ReactDom from "react-dom";
 import "./index.css"
 
 // setup vars
-const firstBook = {
-    img: "https://m.media-amazon.com/images/I/81T1F3Dv0YL._SX140_.jpg",
-    title: "Harry Potter",
-    author: "J.K. Rowling"
-}
+const books = [
+    {
+        id: 1,
+        img: "https://m.media-amazon.com/images/I/81T1F3Dv0YL._SX140_.jpg",
+        title: "Harry Potter",
+        author: "J.K. Rowling"
+    },
+    {
+        id: 2,
+        img: "https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg",
+        title: "A Promised Land",
+        author: "Barack Obama"
+    },
+    {
+        id: 3,
+        img: "https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg",
+        title: "If Animals Kissed Good Night",
+        author: "Ann Whitford Paul"
+    }
+];
 
-const secondBook = {
-    img: "https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg",
-    title: "A Promised Land",
-    author: "Barack Obama"
-}
 
 function BookList() {
     return (
         <section className="booklist">
-            <Book
-                img={firstBook.img}
-                title={firstBook.title}
-                author={firstBook.author}
-            />
-            <Book
-                img={secondBook.img}
-                title={secondBook.title}
-                author={secondBook.author}
-            />
+            {books.map((book) => {
+                return <Book key={book.id} {...book}></Book>;
+            })}
         </section>
-    )
+    );
 }
 
-const Book = ({ img, title, author }) => {
-    // const { img, title, author } = props;
+const Book = (props) => {
+    const { img, title, author } = props;
     return (
         <article className="book">
             <img src={img} alt="" />
