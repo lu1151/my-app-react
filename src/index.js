925c07ce95c0e390af35ba84b8ce566c2ad871a3
lu1151 <luxi.li@outlook.com>
2020-11-05 00:10:37 -0500

    11/05Commit

--- src/index.js
+++ src/index.js
@@ -37,11 +37,24 @@ function BookList() {
 
 const Book = (props) => {
     const { img, title, author } = props;
+    const clickHandler = (e) => {
+        console.log(e);
+        console.log(e.target);
+        alert("Hello");
+    }
+    const more = (author) => {
+        console.log(author);
+    }
+
     return (
-        <article className="book">
+        <article className="book" onMouseOver={() => {
+            console.log(title);
+        }}>
             <img src={img} alt="" />
-            <h3>{title}</h3>
+            <h3 onClick={() => { console.log(title) }}>{title}</h3>
         By <span>{author}</span>
+            <button type="button" onClick={clickHandler}>Button</button>
+            <button type="button" onClick={() => more(author)}>More</button>
         </article>)
 }
 
